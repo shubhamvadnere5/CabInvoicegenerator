0a4ee5e162386e1afb54a56fbcdac58072389ffe
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-22 12:11:58 +0530

    [ShubhamPV] Total fare for multiple rides

--- UnitTestProjectCab/UnitTest1.cs
+++ UnitTestProjectCab/UnitTest1.cs
@@ -24,5 +24,20 @@ namespace UnitTestProjectCab
                 double expected = 25;
             Assert.AreEqual(expected, fare);
             }
+
+        //Uc2 calculate total fare for multiple ride
+        //Given number of rides should return total invoice summery
+        [TestMethod]
+        [TestCategory("Fare")]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] ride = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary actual = invoiceGenerator.CalculateFare(ride);
+            InvoiceSummary expected = new InvoiceSummary(2, 60.0);
+            Assert.AreEqual(expected, actual);
         }
     }
+}
+        
+    
